9dbc0ecdd83c05f3f404f8b2344dbc522c0be177
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2024-05-26 13:24:08 +0000

    [add]chap1

---
+++ chap1_5.php
@@ -0,0 +1,7 @@
+<?php
+var_dump( 10 + 5 );
+var_dump( 10 - 5 );
+var_dump( 10 * 5 );
+var_dump( 10 / 5 );
+var_dump( 2 + 0.5 );
+?>
---
+++ chap1_6.php
@@ -0,0 +1,7 @@
+<?php
+var_dump( 2 + 10 + 5 );
+var_dump( 2 + 10 * 5 );
+var_dump( ( 2 + 10 ) * 5 );
+var_dump( 5 / ( 4 * ( 1 - 0.2 )));
+var_dump( 2 + 10 * -5 );
+?>
